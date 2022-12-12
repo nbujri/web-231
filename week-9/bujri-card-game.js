3a68a424c015835a2af70de53a67f1ff4113a2a1
Ngi Bujri <nbujri7@gmail.com>
2022-12-12 11:00:04 -0500

    setup files

---
+++ week-9/bujri-card-game.js
@@ -0,0 +1,6 @@
+// ====================================================================
+//     ; Title: Assignment 9.2
+//     ; Author: Ngi Bujri
+//     ; Date: 12 December 2022
+//     ; Description: demonstrate JS functions by building a card game
+// ====================================================================
